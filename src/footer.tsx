9cf4a740484ad225f9167507e328c0d108c12a95
Dennis Incollu <d.incollu@gmail.com>
2021-03-12 13:19:02 +0100

    added powered by Algorand in footer

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,13 +1,17 @@
 import {
+  Row, Col,
   Container, Button, Navbar, NavbarBrand, NavbarToggler, Collapse,
   Nav, NavItem, NavLink, NavbarText,
-  UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem
+  UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem,
+  CardText
 } from 'reactstrap'
+
+import AlgorandLogo from './images/Algorand.png'
 
 export const Footer = ({ className }: any) => {
 
   return (
-    <div >
+    <Container>
       <Navbar className={className} color="white" light expand="md">
         <Nav navbar className="mx-auto">
           <NavItem className="mx-3">
@@ -24,9 +28,11 @@ export const Footer = ({ className }: any) => {
           </NavItem>
         </Nav>
       </Navbar>
-      <div className="text-muted text-center">
-        <small>Uncommon Digital SRL © All rights reserved.</small>
-      </div>
-    </div>
+      <Row className="text-muted text-center">
+        <Col>
+          <CardText style={{ fontSize: 14 }}>Uncommon Digital SRL © All rights reserved. Powered by <img src={AlgorandLogo} height="14" alt="Uncommon Creative Logo" /></ CardText>
+        </Col>
+      </Row>
+    </Container>
   )
 }
